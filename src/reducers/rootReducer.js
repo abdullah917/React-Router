e84fabd1b69c324ccd0e5700023ac6cc454d8dd7
abdullah917 <abdullahiqbal442@gmail.com>
2020-10-09 03:35:51 +0500

    first commit

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,25 @@
+const initState = {
+    posts: [
+        {id: '1', title:'Squirtle laid an egg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+        {id: '2', title:'charmander laid an egg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+        {id: '3', title:'A Helix fossil was found', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+    
+    ] 
+
+}
+
+const roootReducer = (state = initState, action) => {
+    if(action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return{
+            ...state,
+            posts: newPosts
+        }
+    }
+    return state;
+}
+
+
+export default roootReducer; 
